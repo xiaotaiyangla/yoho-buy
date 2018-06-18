5b07f0398147330c2227a0e5d85af3f4f7bb151e
rnAx <bhy5259771@163.com>
2018-06-18 15:27:40 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack");
+module.exports = {
+    configureWebpack: {
+        //配置webpack
+        plugins: [
+            new webpack.ProvidePlugin({
+                jQuery: "jquery",
+                $: "jquery"
+            })
+        ]
+    },
+    devServer: {
+        proxy: {
+        '/api': {
+            target: 'http://10.80.13.184:8888/',//我的服务器端口
+            changeOrigin: true,
+            pathRewrite: {
+              '^/api': ''//这里理解成用‘/api'代替target里面的地址，后面组件中我们掉接口时直接用api代替
+            }
+         }
+        }
+    },
+}
