9b870b9dc24098197a8b79c44f0189933cdd905c
riyuejing <culayee@163.com>
2018-06-19 18:35:55 +0800

    头部
    公共部分的头部

--- src/views/personalCenter.vue
+++ src/views/personalCenter.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <title-top :title="登录页面"></title-top>
-        <button @click="goToLast">返回🔙</button>
-        <h1>个人中心</h1>
+        <title-top></title-top>
         <router-link to="/signIn">登录</router-link>
     </div>
 </template>
@@ -11,13 +9,13 @@
     import TitleTop from '../components/common/titleTop'
     export default {
         name: "personalCenter",
+        data(){
+            return{
+                title:"个人中心"
+            }
+        },
         components:{
             TitleTop
-        },
-        methods:{
-            goToLast(){
-                this.$router.go(-1);
-            }
         }
     }
 </script>
