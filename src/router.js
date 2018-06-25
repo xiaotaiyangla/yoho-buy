32054dd43e1a4035310f31297d37c3c2cffb520e
riyuejing <culayee@163.com>
2018-06-25 11:20:55 +0800

    list and search
    list and search

--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ import PersonalCenter from './views/personalCenter/personalCenter'
 import Index from './views/index'
 import Search from './views/search/search'
 import SignIn from './views/signIn'
+import List from './components/common/list/list'
+import ListToCate from './views/lcate'
+import Shop from './views/shop'
 //新品到着页面
 import New from './views/new'
 import SignUp from './views/signUp/signUp'
@@ -60,7 +63,20 @@ export default new Router({
       {
           path:'/search',
           name:'search',
-          component:Search
+          component:Search,
+          children:[
+              {
+                  path:"list",
+                  name:"list",
+                  component:List
+              }
+          ]
+      },
+      //店铺的页面
+      {
+       path:'/shop/:sellerName',
+       name:"shop",
+       component: Shop
       },
       //新品到着页面
       {
@@ -73,6 +89,11 @@ export default new Router({
           path: '/cate',
           name:'cate',
           component:Cate
+      },
+      //分类页面跳到的搜索页面
+      {
+          path:'/list',
+          component:ListToCate
       }
   ]
 })
