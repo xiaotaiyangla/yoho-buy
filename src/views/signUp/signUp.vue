71617d2915ea0d20ce635f21e1dc418f19bd3855
rnAx <bhy5259771@163.com>
2018-06-28 09:26:13 +0800

    修复了合并时的bug

--- src/views/signUp/signUp.vue
+++ src/views/signUp/signUp.vue
@@ -63,7 +63,6 @@
                 showpwd:false,
                 showpwdicon:"icon-htmal5icon08",
                 typepwd:"password",
-                imgsrc:"http://10.80.13.205:3000/captcha"
                 imgsrc:"/api/captcha",
                 flag:"",
                 isRegOk:''
