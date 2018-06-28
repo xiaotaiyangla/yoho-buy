2c3021c81fa02d706cd7fceae9222072f8bdab6f
cczrz123 <1789346378@qq.com>
2018-06-28 09:02:38 +0800

    订单页上传

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ require('./assets/font/iconfont.css')
 require('./assets/font3/iconfont.css')
 require('./assets/font4/iconfont.css')
 require('./assets/font5/iconfont.css')
-
+require('./assets/font6/iconfont.css')
 import axios from 'axios'
 import $ from 'jquery'
 Vue.prototype.$http = axios;
